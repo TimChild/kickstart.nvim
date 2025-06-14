533d4d6c5a30809cdd086b9624e8b4b27a13c5b0
Tim Child <timchild@EXOWATTMAC-TC2.local>
2025-06-14 14:10:01 -0400

    update lsp config for 0.11+

---
+++ lua/custom/plugins/mason.lua
@@ -0,0 +1,22 @@
+-- Installs lsp servers and enables automatically
+return {
+  'mason-org/mason-lspconfig.nvim',
+  dependencies = {
+    { 'mason-org/mason.nvim', opts = {} },
+    'neovim/nvim-lspconfig',
+  },
+  opts = {
+    ensure_installed = {
+      'lua_ls',
+      'pyright',
+      'gopls',
+      'html',
+      'bashls',
+      'dockerls',
+      'yamlls',
+      'stylua', -- Used to format Lua code
+      'prettierd', -- Used to format HTML code
+    },
+    automatic_enable = true,
+  },
+}
