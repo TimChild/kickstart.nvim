f61bfa519b9518a478ee8fd1e8841bc2f30fabac
Tim Child <timjchild@gmail.com>
2025-12-25 13:23:46 -0600

    feat: add Shift+Tab for unindenting
    - Insert mode: Shift+Tab unindents current line
    - Normal mode: Shift+Tab unindents current line
    - Visual mode: Shift+Tab unindents selection and keeps it selected
    - Visual mode: Tab indents selection and keeps it selected
    
    This matches standard editor behavior for indentation controls.

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,6 +1,18 @@
-print 'Setting up keymaps -- 2025-02-01 -- I dont think this is being used...'
+-- Custom keymaps
+
 -- Copy to system clipboard
-vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
+
+-- Shift+Tab to unindent (decrease indent)
+-- Works in insert, normal, and visual modes
+vim.keymap.set('i', '<S-Tab>', '<C-d>', { desc = 'Unindent line' })
+vim.keymap.set('n', '<S-Tab>', '<<', { desc = 'Unindent line' })
+vim.keymap.set('v', '<S-Tab>', '<gv', { desc = 'Unindent selection' })
+
+-- Tab to indent in visual mode (keeps selection)
+vim.keymap.set('v', '<Tab>', '>gv', { desc = 'Indent selection' })
+
 -- 2024-11-06 -- want to add this back in, but something wrong with setup that is causing errors on commenting
 -- and dont want this confusing things further
 --
